7a40e8838e2ed6727ad0cbe7f4a58b040f30a73d
JeffBuck8 <jdbuckcode@gmail.com>
2022-10-11 14:22:39 -0400

    feat: add name placeholder under category

--- cleansweep/src/Leaderboard-Page.js
+++ cleansweep/src/Leaderboard-Page.js
@@ -2,12 +2,13 @@ import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Card from 'react-bootstrap/Card';
+import Stack from 'react-bootstrap/Stack'
 
 function LeaderboardTitles() {
     return (
 
         <Container>
-        <Card.Title className='text-center'>Leaderboards</Card.Title>
+        <Card.Title className='text-center py-3'>Leaderboards</Card.Title>
         <Row>
         <Col className='text-center border py-3' lg={4} md={4}>
             Global
@@ -20,27 +21,27 @@ function LeaderboardTitles() {
         </Col>
         </Row>
 
-        <Row>
-            <Col className='text-center border py-2' xs={6} md={4}>
-            name placeholder
-            </Col>
-            <Col className='text-center border py-2' xs={6} md={4}>
-            name placeholder
-            </Col>
-            <Col className='text-center border py-2'xs={6} md={4}>
-            name placeholder
-            </Col>
-        </Row>
+        <Col className='text-start border py-2 my-1' xs={4} xs={4}>
+        <Stack gap={2}>
+            <div className="bg-light border">1. name placeholder</div>
+            <div className="bg-light border">2. name placeholder</div>
+            <div className="bg-light border">3. name placeholder</div>
+            <div className="bg-light border">4. name placeholder</div>
+            <div className="bg-light border">5. name placeholder</div>
+        </Stack>
+
+            
+        </Col>
 
         <Row>
             <Col className='text-center border py-2' lg={4} md={4}>
-                Friends
+            Friends
             </Col>
         </Row>
 
         <Row>
-        <Col className='text-center border py-2' lg={4} md={4}>
-                name placeholder
+        <Col className='text-start border py-2' lg={4} md={4}>
+            1. name placeholder
         </Col>
         </Row>
         </Container>
