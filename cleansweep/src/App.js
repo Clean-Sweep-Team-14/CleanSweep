d9f21bf9b9c693dcd0a0f41c7bf2f8ea67996a8b
JeffBuck8 <jdbuckcode@gmail.com>
2022-10-11 11:55:52 -0400

    feat: add grid names and placeholders

--- cleansweep/src/App.js
+++ cleansweep/src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import NavBar from 'react-bootstrap/Navbar'
 import LeaderboardTitles from './Leaderboard-Page';
 
 function App() {
   return (
 <div>
     <main>
+    <NavBar></NavBar>
     <LeaderboardTitles></LeaderboardTitles>
     </main>
 </div>
--- cleansweep/src/Leaderboard-Page.js
+++ cleansweep/src/Leaderboard-Page.js
@@ -1,19 +1,47 @@
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import Card from 'react-bootstrap/Card';
 
 function LeaderboardTitles() {
     return (
+
         <Container>
-        <Row className='my-5 py-4'>
-            <Col sm={4}>Global</Col>
-            <Col sm={4}>Monthly</Col>
-            <Col sm={4}>Household</Col>
-        </Row>
+        <Card.Title className='text-center'>Leaderboards</Card.Title>
         <Row>
-            <Col sm>names</Col>
-            <Col sm>names</Col>
-            <Col sm>names</Col>
+        <Col className='text-center border py-3' lg={4} md={4}>
+            Global
+            </Col>
+        <Col className='text-center border py-3' lg={4} md={4}>
+            Monthly
+            </Col>
+        <Col className='text-center border py-3' lg={4} md={4}>
+            Household
+        </Col>
+        </Row>
+
+        <Row>
+            <Col className='text-center border py-2' xs={6} md={4}>
+            name placeholder
+            </Col>
+            <Col className='text-center border py-2' xs={6} md={4}>
+            name placeholder
+            </Col>
+            <Col className='text-center border py-2'xs={6} md={4}>
+            name placeholder
+            </Col>
+        </Row>
+
+        <Row>
+            <Col className='text-center border py-2' lg={4} md={4}>
+                Friends
+            </Col>
+        </Row>
+
+        <Row>
+        <Col className='text-center border py-2' lg={4} md={4}>
+                name placeholder
+        </Col>
         </Row>
         </Container>
     );
