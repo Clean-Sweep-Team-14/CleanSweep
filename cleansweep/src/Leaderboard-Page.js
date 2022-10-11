aec66b4dbffe7316db97355ae5372847febd673d
JeffBuck8 <jdbuckcode@gmail.com>
2022-10-11 16:23:11 -0400

    feat: align categories and fixed columns

--- cleansweep/src/Leaderboard-Page.js
+++ cleansweep/src/Leaderboard-Page.js
@@ -1,51 +1,60 @@
-import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
-import Card from 'react-bootstrap/Card';
+import ListGroup from "react-bootstrap/ListGroup"
+
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Card from "react-bootstrap/Card";
 import Stack from 'react-bootstrap/Stack'
 
-function LeaderboardTitles() {
-    return (
 
-        <Container>
+const LeaderboardTitles = () => (
+    
+<Container>
         <Card.Title className='text-center py-3'>Leaderboards</Card.Title>
         <Row>
-        <Col className='text-center border py-3' lg={4} md={4}>
-            Global
-            </Col>
-        <Col className='text-center border py-3' lg={4} md={4}>
-            Monthly
-            </Col>
-        <Col className='text-center border py-3' lg={4} md={4}>
-            Household
-        </Col>
-        </Row>
-
-        <Col className='text-start border py-2 my-1' xs={4} xs={4}>
-        <Stack gap={2}>
-            <div className="bg-light border">1. name placeholder</div>
-            <div className="bg-light border">2. name placeholder</div>
-            <div className="bg-light border">3. name placeholder</div>
-            <div className="bg-light border">4. name placeholder</div>
-            <div className="bg-light border">5. name placeholder</div>
+        <Stack className='text-center border py-3 col-3'>
+            <div className="border">Global</div>
+            <div className="bg-white my-2">1. name placeholder</div>
+            <div className="bg-white my-2">2. name placeholder</div>
+            <div className="bg-white my-2">3. name placeholder</div>
+            <div className="bg-white my-2">4. name placeholder</div>
+            <div className="bg-white my-2">5. name placeholder</div>
         </Stack>
 
-            
-        </Col>
+        <Stack className='text-center border py-3 col-3'>
+            <div className="border">Monthly</div>
+            <div className="bg-white">1. name placeholder</div>
+            <div className="bg-white">2. name placeholder</div>
+            <div className="bg-white">3. name placeholder</div>
+            <div className="bg-white">4. name placeholder</div>
+            <div className="bg-white">5. name placeholder</div>
+        </Stack>
 
+        <Stack className='text-center border py-3 my- col-3'>
+        <div className="border">Household</div>
+            <div className="bg-white my-2">1. name placeholder</div>
+            <div className="bg-white my-2">2. name placeholder</div>
+            <div className="bg-white my-2">3. name placeholder</div>
+            <div className="bg-white my-2">4. name placeholder</div>
+            <div className="bg-white my-2">5. name placeholder</div>
+        </Stack>
+
+    </Row>
         <Row>
-            <Col className='text-center border py-2' lg={4} md={4}>
-            Friends
-            </Col>
+<Col>
+        <Stack className='text-center border py-3 my-5 col-4'>
+        <div className="border">Friends</div>
+            <div className="bg-white my-2">1. name placeholder</div>
+            <div className="bg-white my-2">2. name placeholder</div>
+            <div className="bg-white my-2">3. name placeholder</div>
+            <div className="bg-white my-2">4. name placeholder</div>
+            <div className="bg-white my-2">5. name placeholder</div>
+        </Stack>
+</Col>   
         </Row>
 
-        <Row>
-        <Col className='text-start border py-2' lg={4} md={4}>
-            1. name placeholder
-        </Col>
-        </Row>
-        </Container>
-    );
-}
+</Container>
+
+);
 
 export default LeaderboardTitles;
