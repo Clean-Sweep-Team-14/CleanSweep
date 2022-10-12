747bca038b3bd2b53f2a7360a2afdec89a3ca800
JeffBuck8 <jdbuckcode@gmail.com>
2022-10-12 11:27:20 -0400

    feat: add login, register, logout roots and encode/decode util

--- cleansweep/src/Endpoints.js
+++ cleansweep/src/Endpoints.js
@@ -1,7 +1,36 @@
 import axios from "axios"
+import { encodeUsername } from "./utils"
 
 const apiRoot = 'https://clean-sweep-team-14.herokuapp.com'
 
 const urls = {
-    
+    login: () => `${apiRoot}/auth/token/login/`,
+    register: () => `${apiRoot}/auth/users/`,
+    logout: () => `${apiRoot}/auth/token/logout/`,
+}
+
+const login = async (body) => {
+    body.username = encodeUsername(body.username.trim())
+    return axios.post(urls.login(), body)
+}
+
+const register = async (body) => {
+    body.username = encodeUsername(body.username.trim())
+    return axios.post(urls.register(), body)
+}
+
+const logout = async (token) => {
+    return axios.post(urls.logout(), '', {
+        headers: {
+            Authorization: `Token ${token}`,
+        },
+    })
+}
+
+
+export {
+    login,
+    register,
+    logout,
+    urls,
 }
