a30172b9a151e423d66d029c69c9a18ef06c0316
GitLukeW <gitlukew@gmail.com>
2022-10-14 18:42:02 -0400

    setting up celery and redis

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -15,6 +15,7 @@ from pathlib import Path
 import django_on_heroku
 
 
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -155,3 +156,8 @@ REST_FRAMEWORK = {
         'rest_framework.permissions.IsAuthenticatedOrReadOnly',
     ]
 }
+
+
+CELERY_BROKER_URL = env('REDIS_URL')
+CELERY_ACCEPY_CONTENT = ['json']
+CELERY_TASK_SERIALIZER = 'json'
