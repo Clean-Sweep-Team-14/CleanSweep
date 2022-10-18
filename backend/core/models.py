b045943b89b3af61097073bafbfaebceeaa328e4
Ajsteph89 <ajsteph89@gmail.com>
2022-10-18 16:52:19 -0400

    points are able to be deducted but not as originally planned

--- backend/core/models.py
+++ backend/core/models.py
@@ -4,6 +4,8 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 from django.db.models import Sum, Q
 from django.db.models.constraints import UniqueConstraint
+import datetime
+import pytz
 
 
 
@@ -12,11 +14,21 @@ class CustomUser(AbstractUser):
     
     avatar_picture = models.ImageField(upload_to = 'users', blank=True, null=True)
 
-    @property
     def total_points(self):
-        return self.choretrackers.filter(complete=True).aggregate(Sum('chore__point', default=0))
+        chores_points=[tracker.chore.point for tracker in self.choretrackers.all() if not tracker.is_late]
+        positive_points = sum(chores_points)
+        return positive_points
 
-    pass
+        # return self.choretrackers.filter(completed_at=True).aggregate(Sum('chore__point', default=0))
+
+    def point_deduction(self):
+        late_chores_points=[tracker.chore.point for tracker in self.choretrackers.all() if tracker.is_late]
+        negative_points = sum(late_chores_points)
+        return negative_points
+
+    def actual_points(self):
+        return self.total_points() - self.point_deduction()
+
 
     def __str__(self):
         return self.username
@@ -46,8 +58,16 @@ class Chore(models.Model):
 class Chore_Tracker(models.Model):
     chore = models.ForeignKey(Chore, on_delete=models.CASCADE, related_name = 'choretrackers')
     day = models.DateTimeField()
-    complete = models.BooleanField(default=False)
+    completed_at = models.DateTimeField(default=datetime.datetime.now(), null=True, blank=True)
     user = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name = 'choretrackers')
+
+    @property
+    def is_late(self):
+        return self.day <= self.completed_at
+        
+        
+        #  pytz.utc.localize(datetime.datetime.today ())
+
 
     def __str__(self):
         return f'{self.chore} on {self.day}'
