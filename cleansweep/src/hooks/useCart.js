b28570b96cd0e1099b4eac1348650deb34c99510
JeffBuck8 <jdbuckcode@gmail.com>
2022-10-19 17:04:46 -0400

    deleted sudo code

--- cleansweep/src/hooks/useCart.js
+++ cleansweep/src/hooks/useCart.js
@@ -23,34 +23,6 @@ export const CartProvider = ({ children }) => {
     setCart(scopedCart);
   };
 
-  //   const login = async (username, password) => {
-  //     const body = {
-  //       username,
-  //       password,
-  //     };
-
-  //     setLoading(true);
-
-  //     try {
-  //       const response = await apiEndpoints.login(body);
-  //       setLoading(false);
-
-  //       if (response.status === 200) {
-  //         // This should be data returned from the server instead of shaping the data yourself
-
-  //         const userData = {
-  //           username,
-  //           auth_token: response.data.auth_token,
-  //         };
-
-  //         setUser(userData);
-  //         navigate("/");
-  //       }
-  //     } catch (err) {
-  //       console.log(err);
-  //       setLoading(false);
-  //     }
-  //   };
 
   const memoedValue = useMemo(
     () => ({
