06ab288cc425958d8da0dd88faeea039372410f4
Gary Whitford <ssrdunc@gmail.com>
2022-10-20 17:16:28 -0400

    changed total_points to actual_points in Leaderboards to match backend

--- cleansweep/src/Components/LeaderBoards.js
+++ cleansweep/src/Components/LeaderBoards.js
@@ -35,15 +35,15 @@ export default function Leaderboards() {
         <LeaderBoardColumn
           title="Global"
           leaders={allGlobalLeadersData
-            .sort((a, b) => (a.total_points < b.total_points ? 1 : -1))
-            .map((item) => `${item.username} ${item.total_points}`)
+            .sort((a, b) => (a.actual_points < b.actual_points ? 1 : -1))
+            .map((item) => `${item.username} ${item.actual_points}`)
             }
         />
         <LeaderBoardColumn
           title="Friends"
           leaders={allFriendsLeadersData
-            .sort((a, b) => (a.total_points < b.total_points ? 1 : -1))
-            .map((item) => `${item.username} ${item.total_points}`)
+            .sort((a, b) => (a.actual_points < b.actual_points ? 1 : -1))
+            .map((item) => `${item.username} ${item.actual_points}`)
             }
         
 
