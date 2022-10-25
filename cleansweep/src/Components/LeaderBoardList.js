e0e10b6949cf2098f87b537a7fa04e8acf576562
Ajsteph89 <ajsteph89@gmail.com>
2022-10-25 19:20:51 -0400

    emojis added

--- cleansweep/src/Components/LeaderBoardList.js
+++ cleansweep/src/Components/LeaderBoardList.js
@@ -5,7 +5,7 @@ export default function LeaderBoardList(props) {
     <ListGroup 
       id = 'leaderBoard'
       className="py-2" as='ol' numbered
-      style={{ width: "40%", margin: "0 auto"}}
+      style={{ width: "50%", margin: "0 auto"}}
     >
       {props.items.map((item) => (
         <ListGroup.Item style={{display:'flex', justifyContent: 'space-between'}} key={item.username}><div style={{flexGrow: 1}}>{item.username}</div><div>{item.actual_points}</div></ListGroup.Item>
