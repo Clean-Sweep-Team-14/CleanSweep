2b1309d68120c460b045d39def1fe9b29bf50fd2
Ajsteph89 <ajsteph89@gmail.com>
2022-10-25 14:47:46 -0400

    added instructional header

--- cleansweep/src/Components/Pages/MyChores/index.js
+++ cleansweep/src/Components/Pages/MyChores/index.js
@@ -65,7 +65,7 @@ const MyChores = () => {
 
   return (
     <Page title="My Chores" totalPoints={user.totalPoints}>
-      <p style={{textAlign: "center", color: '#f44336'}}>Do your chores by the due date or you will lose points!</p>
+      <p style={{textAlign: "center", color: '#f44336'}}>Do your chores by the due date or you will lose points, ya filty animal!</p>
       <Row>
         <LeaderBoardColumn
           leaders={myChores.map((item) => {
