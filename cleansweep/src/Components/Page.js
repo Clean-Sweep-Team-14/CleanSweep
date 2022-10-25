7de26196bc4578167201632e4ccf94c87c5913c3
JeffBuck8 <jdbuckcode@gmail.com>
2022-10-25 18:22:04 -0400

    fix: fix total user points on login

--- cleansweep/src/Components/Page.js
+++ cleansweep/src/Components/Page.js
@@ -1,17 +1,41 @@
 import React, { useEffect } from "react";
 import Container from "react-bootstrap/Container";
 import allGlobalLeadersData from "../Components/Pages/Chores/index";
+import { useState } from "react";
+import * as apiEndpoints from "../Endpoints";
+import useAuth from "../hooks/useAuth";
 
 export default function Page(props) {
+  const { user } = useAuth();
+  const [userPoints, setUserPoints] = useState({});
+
   useEffect(() => {
     document.title = `${props.title} | CleanSweep`;
     window.scrollTo(0, 0);
+    fetchUserData();
   }, [props.title]);
+
+  const fetchUserData = async () => {
+    try {
+      const response = await apiEndpoints.getListGlobalLeaderboard();
+
+      if (response.status === 200 && user) {
+        const userData = response.data.results.find(
+          (data) => data.username === user.username
+        );
+        const updatedUser = { ...user, totalPoints: userData.actual_points };
+        setUserPoints(updatedUser);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <Container className="p-3">
       <h1>{props.title} </h1>
       <h2 className="text-end">
-        My Total Points: {`${props.totalPoints || 0}`}
+        My Total Points: {`${userPoints.totalPoints || 0}`}
       </h2>
       <hr />
       {props.children}
