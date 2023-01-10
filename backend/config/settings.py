a3ceafc2a90c120f50b049240aaad2220f823cd0
GitLukeW <gitlukew@gmail.com>
2023-01-10 16:20:01 -0500

    working on moving from heroku to render

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -161,3 +161,10 @@ CELERY_BROKER_URL = 'redis://localhost:6379'
 CELERY_RESULT_BACKEND = 'redis://localhost:6379'
 CELERY_ACCEPT_CONTENT = ['json']
 CELERY_TASK_SERIALIZER = 'json'
+
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.TokenAuthentication',
+    ),
+}
